e50f21aa3f4dbc81e0744896c4ccb08ad7175bda
Richard O'Grady <ogradyir@gmail.com>
2015-05-07 01:38:49 -0700

    Modified enternatiment center library to read from an external file.

--- final/entertainment_center.py
+++ final/entertainment_center.py
@@ -1,7 +1,23 @@
 import fresh_tomatoes
 import media
 
-space_jam = media.Movie("Space Jam", "images/space-jam.jpg", "https://www.youtube.com/watch?v=u7AYd8uGG6E", ["Michael Jordan", "Bugs Bunny"], "November 15, 1996", 3)
+with open("bestFilms.txt") as fin:
+    rawData = fin.readlines()
 
+rawData = [element.strip('\n') for element in rawData]
 
-fresh_tomatoes.open_movies_page([space_jam])
+movieData = []
+
+movieData = [element.split('; ') for element in rawData]
+
+for element in movieData:
+    element[1] = "images/"+element[1]
+    element[2]= "https://www.youtube.com/watch?v="+element[2]
+    element[3] = element[3].split(', ')
+
+movieList = []
+
+for element in movieData:
+    movieList.append( media.Movie(element[0],element[1],element[2],element[3],element[4],element[5]) )
+
+fresh_tomatoes.open_movies_page(movieList)
